b40b7dc52d82b3fe01d95820490c9510d2cfa90a
wild-mental <wild.mental@kidsnote.com>
2024-02-19 05:48:30 +0900

    JS 5일차 수업용 준비 코드 커밋 10 - 백그라운드 이동 방식 예제 개선

---
+++ 4. 웹 API/3. DOM 게임 제작 : 스프라이트 무빙 액션/3-2. 백그라운드 무빙/gameScript.js
@@ -0,0 +1,98 @@
+'use strict'
+
+// 1. 캐릭터 이미지 사전로드
+const imgDir = '../img_character/';
+const postureTypes = ['back', 'front', 'left', 'right']
+const images = Array.from({length:40},
+    (_, idx) => `${imgDir}${postureTypes[Math.floor(idx/10)]}_walk${idx%10}.png`
+)
+images.forEach(image => {
+    const img = new Image();
+    img.src = image;
+});
+
+// 2. 캐릭터 요소 선택
+const $townMap = document.getElementsByClassName('town-map')[0];
+const $userCharacter = document.getElementById('user-character');
+document.addEventListener('DOMContentLoaded', function (){
+        $userCharacter.setAttribute('style', `background-image: url('${imgDir}front_stand.png');`)
+    }
+)
+
+// 3. 캐릭터 이동 관련 변수 및 메서드 선언
+const characterLocation = {
+    x: 0, y: 0
+}
+const step = 20;
+const animationPostfix = {
+    ArrowDown: 'forward',
+    ArrowUp: 'backward',
+    ArrowLeft: 'left',
+    ArrowRight: 'right'
+}
+const moveTo = {
+    ArrowDown: (function forward() { return function () { characterLocation.y += step; } })(),
+    ArrowUp: (function backward() { return function(){ characterLocation.y -= step; } })(),
+    ArrowLeft: (function forward() { return function(){ characterLocation.x -= step; } })(),
+    ArrowRight: (function forward() { return function(){ characterLocation.x += step; } })()
+}
+const runningActions = Object.values(animationPostfix).map(direction => `running-${direction}`);
+const pausedActions = Object.values(animationPostfix).map(direction => `pause-${direction}`);
+function playAnimation(key) {
+    $userCharacter.classList.remove(...pausedActions);
+    $userCharacter.classList.add(`running-${animationPostfix[key]}`);
+}
+function pauseAnimation(key) {
+    $userCharacter.classList.remove(...runningActions);
+    $userCharacter.classList.add(`pause-${animationPostfix[key]}`);
+}
+
+// 4. 캐릭터 이동 제어를 위한 방향키 이벤트 리스너 부착
+document.addEventListener('keydown', (event) => {
+    // 4-1. 이벤트 키 검사
+    if (!Object.keys(animationPostfix).includes(event.key)){
+        return;
+    }
+
+    // 4-2. 키 입력에 따른 캐릭터 좌표 수정
+    // switch (key) {
+    //     case 'ArrowUp':
+    //         if (characterLocation.y !== 0) {
+    //             characterLocation.y -= step;
+    //         }
+    //         break;
+    //     case 'ArrowDown':
+    //         characterLocation.y += step;
+    //         break;
+    //     case 'ArrowLeft':
+    //         if (characterLocation.x !== 0) {
+    //             characterLocation.x -= step;
+    //         }
+    //         break;
+    //     case 'ArrowRight':
+    //         characterLocation.x += step;
+    //         break;
+    //     default:
+    //         break;
+    // }
+
+    // 4-3. 함수형 프로그래밍 : 함수객체 적용 버전으로 개선
+    moveTo[event.key]();
+
+    // 4-4. 키 입력에 따른 캐릭터 애니메이션 재생시작
+    setTimeout(() => {playAnimation(event.key)}, 100);
+
+    // 4-5. 타운맵 이동
+    $townMap.setAttribute('style', `background-position: ${-characterLocation.x}px ${-characterLocation.y}px`);
+
+});
+
+// 5. 방향키 입력 종료 감지 리스너 부착
+document.addEventListener('keyup', (event) => {
+    // 5-1. 이벤트 키 검사
+    if (!Object.keys(animationPostfix).includes(event.key)){
+        return;
+    }
+    // 5-2. CSS 애니메이션 일시정지
+    setTimeout(() => {pauseAnimation(event.key)}, 200);
+});
